e48ab51d906b275cc5dd0b2976c90c38e60bf683
aqzp <72946059+AsynchronousAI@users.noreply.github.com>
2024-02-19 12:48:02 -0600

    Use CDN

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import * as fs from 'fs';
 import * as https from 'https';
-https.globalAgent.options.ca = fs.readFileSync('./ca_intermediate_root_bundle.pem');
+import fetch from 'node-fetch';
+
+fetch("https://raw.githubusercontent.com/AsynchronousAI/aki-api-nextjs/develop/src/ca_intermediate_root_bundle.pem")
+  .then(response => response.text())
+  .then(file => {
+    https.globalAgent.options.ca = file
+  })
 
 import Aki, { answers } from './Akinator';
 import { regions, region } from './constants/Client';
